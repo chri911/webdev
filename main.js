80a5285e0bc34edeb8e7ef4ee9efea66c1027d29
chri911 <delightsome@bigmir.net>
2020-09-25 12:24:40 +0300

    feat: fix sections

--- main.js
+++
@@ -1,9 +0,0 @@
-$('.slider__items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    arrows: true,
-    appendArrows: $('.my_arrows'),
-    nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
-    prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-  });
